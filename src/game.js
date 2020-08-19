a995e1246770b411bdd8839d22314e2809e95c08
Jexxie <1989wjx@gmail.com>
2020-08-19 11:05:30 +0800

    first commit

---
+++ src/game.js
@@ -0,0 +1,54 @@
+import {
+  update as updateSnake,
+  draw as drawSnake,
+  SNAKE_SPEED,
+  getSnakeHead,
+  snakeIntersection,
+} from "./snake.js";
+import { update as updateFood, draw as drawFood } from "./food.js";
+import { outsideGrid } from "./grid.js";
+
+let lastRenderTime = 0;
+
+let gameOver = false;
+
+const gameBoard = document.getElementById("game-board");
+
+function main(currentTime) {
+  if (gameOver) {
+    if (confirm("游戏结束，点确定便重新开始~")) {
+      window.location = "/src/index.html";
+    }
+    return;
+  }
+  window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+
+  lastRenderTime = currentTime;
+
+  update();
+  draw();
+}
+
+window.requestAnimationFrame(main);
+
+setTimeout(function() {window.alert("按←↑↓→方向键控制")},500)
+
+
+
+function update() {
+  updateSnake();
+  updateFood();
+  checkDeath();
+}
+
+function draw() {
+  gameBoard.innerHTML = "";
+  drawSnake(gameBoard);
+  drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
+}
